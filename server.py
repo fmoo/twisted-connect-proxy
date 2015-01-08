1197bb73af922ec9e16b2c2b9e7238fadbf71197
Remi Rampin <remirampin@gmail.com>
2015-01-08 19:33:08 +0100

    Adds --ssl-key for separate cert/key files

--- server.py
+++ server.py
@@ -124,6 +124,7 @@ if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument('port', default=8080, nargs='?', type=int)
     ap.add_argument('--ssl-cert', type=str)
+    ap.add_argument('--ssl-key', type=str)
     ns = ap.parse_args()
 
     import twisted.web.http
@@ -131,11 +132,23 @@ if __name__ == '__main__':
     factory.protocol = ConnectProxy
 
     import twisted.internet
+    if ns.ssl_key and not ns.ssl_cert:
+        log.msg("--ssl-key must be used with --ssl-cert")
+        sys.exit(1)
     if ns.ssl_cert:
         from twisted.internet import ssl
         with open(ns.ssl_cert, 'rb') as fp:
             ssl_cert = fp.read()
-        certificate = ssl.PrivateCertificate.loadPEM(ssl_cert)
+        if ns.ssl_key:
+            from OpenSSL import crypto
+            with open(ns.ssl_key, 'rb') as fp:
+                ssl_key = fp.read()
+            certificate = ssl.PrivateCertificate.load(
+                    ssl_cert,
+                    ssl.KeyPair.load(ssl_key, crypto.FILETYPE_PEM),
+                    crypto.FILETYPE_PEM)
+        else:
+            certificate = ssl.PrivateCertificate.loadPEM(ssl_cert)
         twisted.internet.reactor.listenSSL(ns.port, factory,
                                            certificate.options())
     else:
